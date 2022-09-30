b22aac14d5df270651bb9ddfdfe815c8e1966282
Leonid Haidash <Gaydashleo@gmail.com>
2022-09-30 21:57:41 +0300

    End

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,18 +4,19 @@ import { ContactForm } from 'components/ContactForm/ContactForm';
 import { Filter } from 'components/Filter/Filter';
 import { ContactList } from 'components/ContactList/ContactList';
 import { Container, Section, Titleh1, Titleh2 } from './App.styled';
-import { useEffect } from 'react';
+import { useState,useEffect } from 'react';
 
-export function App()  {
-  const [contacts, setContacts] = useState([{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },]);
+export function App() {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },]);
   
   const [filter, setFilter] = useState('');
 
 
-// Перевіряємо localStorage чи є контакти
+  // Перевіряємо localStorage чи є контакти
   useEffect(() => {
     const contactsStorage = localStorage.getItem('contacts');
     const parsedContacts = JSON.parse(contactsStorage);
@@ -26,73 +27,70 @@ export function App()  {
     }
   }, []);
     
-// Додає контакт в localStorage
+  // Додає контакт в localStorage
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
-    // Додає контакт в список
-  addContact = ({ name, number }) => {
-    const normalizedFind = name.toLowerCase();
-    const findName = this.state.contacts.find(
-      contact => contact.name.toLowerCase() === normalizedFind
+  // Додає контакт в список
+
+   const addContact = ({ name, number }) => {
+    const findName = contacts.find(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
     );
     if (findName) {
       return alert(`${name} is already in contacts.`);
     }
 
-    const findNumber = this.state.contacts.find(
-      contact => contact.number === number
-    );
+    const findNumber = contacts.find(contact => contact.number === number);
     if (findNumber) {
       return alert(`This phone number is already in use.`);
     }
 
-    this.setState(({ contacts }) => ({
-      contacts: [{ name, number, id: nanoid() }, ...contacts],
-    }));
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    setContacts(contacts => [...contacts, newContact]);
   };
 
-  // Вертає результат фільтра
-  getContacts = () => {
-    const { filter, contacts } = this.state;
-    const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
+    // Вертає результат фільтра
+    const getContacts = () => {
+      const normalizedFilter = filter.toLowerCase();
+      return contacts.filter(contact =>
+        contact.name.toLowerCase().includes(normalizedFilter));
+    };
+
+    const visibleContacts = getContacts();
+
+    // Видаляємо контакт з списку
+ const deleteContact = contactId => {
+    setContacts(contacts =>
+      contacts.filter(contact => contact.id !== contactId)
     );
   };
 
-  // Видаляємо контакт з списку
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
+  const handleFilter = e => {
+    setFilter(e.currentTarget.value);
   };
-
-  handleFilter = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
-  };
-
-  render() {
-    const { filter } = this.state;
-    const visibleContacts = this.getContacts();
-
+  
     return (
       <Container>
         <Section title="Phonebook">
           <Titleh1>Phonebook</Titleh1>
-          <ContactForm onSubmit={this.addContact} />
+          <ContactForm onSubmit={addContact} />
         </Section>
         <Section title="Contacts">
           <Titleh2>Contacts</Titleh2>
-          <Filter value={filter} onChange={this.handleFilter} />
+          <Filter value={filter} onChange={handleFilter} />
           <ContactList
             contacts={visibleContacts}
-            onDeleteContact={this.deleteContact}
+            onDeleteContact={deleteContact}
           />
         </Section>
       </Container>
     );
   }
-}
+
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,3 +1,4 @@
+
 import PropTypes from 'prop-types';
 import { Label, Input } from './Filter.styled';
 
